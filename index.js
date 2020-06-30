275cc4cca37f92064464c352852c4080209d458a
邓小宇 <625071541@qq.com>
2020-06-30 13:12:25 +0800

    2020-6-30

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-require('./reset.less');
+import './reset.less';
 
 function setHtmlFontSize(){
-	var dpr = window.devicePixelRatio;
-	var scale = 1/dpr;
-	var metas = document.querySelectorAll("meta");   //meta元素
-	var meta = null;
-	for(var i=0; i<metas.length-1; i++){
-		var name = metas[i].getAttribute('name');
-		if(name && name==='viewport'){
-			meta = metas[i];
-		}
-	}
-	if(!meta){
-		meta = document.createElement("meta");
-		meta.setAttribute("name","viewport");
-		var head = document.querySelector("head");
-		head.appendChild(meta);
-	}
-	meta.setAttribute("content",'user-scalable=no, initial-scale='+scale+', maximum-scale='+scale+', minimum-scale='+scale);
 	var html_width = document.documentElement.clientWidth;
 	var html = document.querySelector("html");
-	let rem = '';
+	let rem = '',fontSize = '';
 	if(window.screen.width>750){
-		rem = html_width/40+"px";
+		rem = html_width/25+"px";
+		fontSize = html_width/75+"px";
 	}else{
 		rem = html_width/10+"px";
+		fontSize = html_width/25+"px";
 	}
 	html.style.fontSize = rem;
+	document.body.style.fontSize = fontSize;
 }
 setHtmlFontSize();
 window.addEventListener("resize",setHtmlFontSize);
